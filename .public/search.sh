ece7ecf783417425fb63533020e5ea3f2b61555f
Robert Winterstein <shizukesa4@gmail.com>
2015-05-19 14:21:31 -0600

    Tue May 19 14:21:31 MDT 2015

--- .public/search.sh
+++ .public/search.sh
@@ -9,6 +9,8 @@ fi
 stack+=$(ag -p $XDG_CONFIG_HOME/ag/agignore -fli --hidden -g "" ~ /etc 2>/dev/null | sed -e 's/^/file:\t/')
 # man page search
 stack+=$(/usr/bin/man -k "" | awk '{print $1,$2}' | sed -e 's/^/man:\t/')
+# process search
+#stack+=$(ps -eo "%p : %U %c %a" | sed -e 's/^/proc:\t/')
 
 # will probably choke on a file / folder with a space in it!
 selection=$(fzf -m --select-1 --exit-0 <<< "$stack" | sed -e 's/[\t|\ |)]//g' -e 's/(/:/g')
@@ -51,6 +53,12 @@ do
             fi
 
             python2 -c "from neovim import attach; nvim=attach('socket', path='$XDG_RUNTIME_DIR/man-$(hostname)'); nvim.command('Man $target'); nvim.command('only');"
+            ;;
+        "proc")
+            if [ "x$target" != "x" ]
+            then
+                kill -${1:-9} $target
+            fi
             ;;
     esac
 done
