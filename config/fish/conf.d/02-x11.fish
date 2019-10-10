341d57752e4bd721bcf979a0c7d528c8d8728da7
Robert Winterstein <shizukesa4@gmail.com>
2019-10-09 21:18:24 -0600

    switch to sway

--- config/fish/conf.d/02-x11.fish
+++ config/fish/conf.d/02-x11.fish
@@ -1,7 +1,10 @@
 if status is-login
     if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-        set -e SESSION_MANAGER
-        set -x DISPLAY ":$XDG_VTNR"
-        xinit "$XINITRC" -- $DISPLAY vt"$XDG_VTNR" -keeptty -br
+        #set -e SESSION_MANAGER
+        #set -x DISPLAY ":$XDG_VTNR"
+        #xinit "$XINITRC" -- $DISPLAY vt"$XDG_VTNR" -keeptty -br
+        cat $XDG_CONFIG_HOME/sway/template | sed 's/#000000/'"$COLOR_DARK"'/g; s/#888888/'"$COLOR_MEDIUM"'/g; s/#FFFFFF/'"$COLOR_LIGHT"'/g; s/#FF0000/'"$COLOR_ACCENT"'/g;' > $XDG_RUNTIME_DIR/swayconfig
+
+        exec sway -c $XDG_RUNTIME_DIR/swayconfig
     end
 end
