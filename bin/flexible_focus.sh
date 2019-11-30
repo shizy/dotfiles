642c659d8630c9d02374836452fc249ca4e6f2df
Robert Winterstein <shizukesa4@gmail.com>
2019-11-29 20:52:56 -0700

    flexible_focus fix for sway

--- bin/flexible_focus.sh
+++ bin/flexible_focus.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
 #get active workspace
-wsnum=$(i3-msg -t get_workspaces | jq '.[] | select(.focused == true).name' | grep -oP '\d+:')
+wsnum=$(swaymsg -t get_workspaces | jq '.[] | select(.focused == true).name' | grep -oP '\d+:')
 
 #get count of open windows in workspace
-count=$(i3-msg -t get_tree | jq --arg ws "$wsnum" '.. | select(.type? == "workspace" and (.name | contains($ws))).nodes | length')
+count=$(swaymsg -t get_tree | jq --arg ws "$wsnum" '.. | select(.type? == "workspace" and (.name | contains($ws))).nodes | length')
 
 if [[ $count -gt 1 ]]; then
-    i3-msg focus right
+    swaymsg focus right
 else
-    i3-msg workspace back_and_forth
+    swaymsg workspace back_and_forth
 fi
